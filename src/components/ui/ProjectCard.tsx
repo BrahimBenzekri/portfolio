6b90529699f8e1a7f33ee448e49006590b496be4
BrahimBenzekri <benzbra04@gmail.com>
2026-04-24 17:23:48 +0100

    Updated: numbering locatiotn in teht project card

--- src/components/ui/ProjectCard.tsx
+++ src/components/ui/ProjectCard.tsx
@@ -42,9 +42,9 @@ export function ProjectCard({ project, index }: ProjectCardProps) {
       {/* Editorial number */}
       {num && (
         <span
-          className={`absolute top-4 right-4 text-3xl font-black font-mono select-none z-10 transition-colors duration-200 ${!showPlaceholder
-              ? "text-black/80 group-hover:text-black"
-              : "text-white/20 group-hover:text-accent/40"
+          className={`absolute top-4 left-4 text-3xl font-black font-mono select-none z-10 transition-colors duration-200 ${!showPlaceholder
+            ? "text-black/80 group-hover:text-black"
+            : "text-white/20 group-hover:text-accent/40"
             }`}
         >
           {num}
